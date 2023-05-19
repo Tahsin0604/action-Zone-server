3087820fdcf0e41382f705686a211f1b225b79ee
Natik Alam Tahsin <natikalamtahsin@gmail.com>
2023-05-19 19:51:27 +0600

    server add

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //import
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -10,6 +11,58 @@ const port = process.env.PORT || 3000;
 //middleware
 app.use(express.json());
 app.use(cors());
+
+//MongoDb server
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hrqu461.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const toyCollection = client.db("actionZoneDB").collection("toys");
+
+    app.get("/toys/:category", async (req, res) => {
+      const page = parseInt(req.query.page) || 0;
+      const limit = parseInt(req.query.limit) || 8;
+      const skip = page * limit;
+      const category = req.params.category;
+      console.log(page, limit, category);
+      let query = {};
+      if (category !== "all") {
+        query = { ub_category: category };
+      }
+
+      const result = await toyCollection
+        .find()
+        .skip(skip)
+        .limit(limit)
+        .toArray();
+
+      console.log(result.length);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Toy store server is running");
