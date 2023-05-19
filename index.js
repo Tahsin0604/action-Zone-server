3f6f96998f890a81dc93266fc88769403f2bbc1e
Natik Alam Tahsin <natikalamtahsin@gmail.com>
2023-05-19 19:09:57 +0600

    server setup

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-//hello
+//import
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+//variable
+const app = express();
+const port = process.env.PORT || 3000;
+
+//middleware
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Toy store server is running");
+});
+app.listen(port, () => {
+  console.log("Port: ", port);
+});
